5a3b61c33c41f21c45713fd869feadea1589f457
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-02-07 18:19:15 +0100

    modifica file server con libreria express e con connessione database

--- server.js
+++ server.js
@@ -1,32 +1,104 @@
+//libreria express
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const http = require("http");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+const path = require('path');
+app.use("/", express.static(path.join(__dirname, "public")));
+
+//libreria mysql2
 const fs = require('fs');
 const mysql = require('mysql2');
-
 const conf = JSON.parse(fs.readFileSync('conf.json'));
+conf.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
 const connection = mysql.createConnection(conf);
 
-// Esegue una query SQL e restituisce una Promise
 const executeQuery = (sql) => {
-   return new Promise((resolve, reject) => {
-      connection.query(sql, function (err, result) {
-         if (err) {
-            console.error(err);
-            reject();
-         }
-         console.log('Query eseguita con successo');
-         resolve(result);
-      });
-   });
-};
+    return new Promise((resolve, reject) => {
+        connection.query(sql, function (err, result) {
+            if (err) {
+                console.error(err);
+                reject();     
+            }   
+            console.log('done');
+            resolve(result);         
+        });
+    })
+}
 
-// Creazione della tabella TODO (se non esiste)
 const createTable = () => {
-   return executeQuery(`
-      CREATE TABLE IF NOT EXISTS todo (
-         id INT PRIMARY KEY AUTO_INCREMENT, 
-         name VARCHAR(255) NOT NULL, 
-         completed BOOLEAN
-      ) 
-   `);
-};
+    return executeQuery(`
+    CREATE TABLE IF NOT EXISTS todo
+        ( id INT PRIMARY KEY AUTO_INCREMENT, 
+          name VARCHAR(255) NOT NULL, 
+          completed BOOLEAN ) 
+       `);      
+}
 
-module.exports = { connection, executeQuery, createTable };
+const insert = (todo) => {
+    const template = `
+    INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED')
+       `;
+    let sql = template.replace("$NAME", todo.name);
+    sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+    return executeQuery(sql); 
+}
+
+const select = () => {
+    const sql = `
+    SELECT id, name, completed FROM todo 
+       `;
+    return executeQuery(sql); 
+}
+
+createTable().then(() => {
+    insert({name: "test " + new Date().getTime(), completed: false}).then((result) => {
+        select().then(console.log);
+    });
+});
+
+
+let todos = [];
+
+app.post("/todo/add", (req, res) => {
+   const todo = req.body.todo;
+   todo.id = "" + new Date().getTime();
+   todos.push(todo);
+   res.json({result: "Ok"});
+});
+
+app.get("/todo", (req, res) => {
+   res.json({todos: todos});
+});
+
+app.put("/todo/complete", (req, res) => {
+    const todo = req.body;
+    try {
+        todos = todos.map((element) => {
+        if (element.id === todo.id) {
+            element.completed = true;
+        }
+        return element;
+        })
+    } catch (e) {
+       console.log(e);
+    }
+    res.json({result: "Ok"});
+});
+
+app.delete("/todo/:id", (req, res) => {
+    todos = todos.filter((element) => element.id !== req.params.id);
+    res.json({result: "Ok"});  
+})
+
+const server = http.createServer(app);
+
+server.listen(5500, () => {
+  console.log("- server running");
+});
